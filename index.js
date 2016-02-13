74394bfa07205bb5ba43fad3cf8d5ff51a3e5914
Martin Hecher <martin.hecher@fraunhofer.at>
2016-02-13 20:34:01 +0000

    adding E57 file information to buildm (WIP)

--- index.js
+++ index.js
@@ -2,44 +2,51 @@ var metadataFilePath = './data/2016-02-13_metadata.xlsx',
   desc2buildm = require('./lib/desc2buildm'),
   Converter = require('./lib/converter'),
   _ = require('underscore'),
-  path = require('path'),
-  fs = require('fs');
+  util = require('util');
 
 var converter = new Converter({
     metadataFilePath: metadataFilePath,
     desc2buildm: desc2buildm
   }),
   cols = ['AA', 'AD', 'AI', 'AJ', 'AK', 'AL', 'AV', 'AZ', 'BA', 'BC', 'BL', 'BM', 'BN', 'BQ', 'BS', 'BT', 'BU', 'BW', 'BZ', 'CD', 'CI'];
+  // cols = ['AD'];
 
 _.forEach(cols, function(col) {
-  var dataset = converter.getDatasetFromSheet('MetaData', col, '10', '32'),
+  var dataset = converter.getDatasetFromSheet('MetaData', col, '9', '32'),
     name = dataset.name;
 
   if (name) {
     console.log('Processing physical asset: ' + name);
 
-    var buildm = converter.dataset2JSONLD(dataset);
+    var paJsonLD = converter.createPhysicalAssetAsJsonLD(dataset);
 
-    converter.jsonld2nquads(buildm.jsonld).then(function(ntripleString, uri) {
-      try {
-        fs.mkdirSync('./output');
-      } catch (err) {};
+    var dosJsonLD = [];
 
-      var outputFile = path.join('./output', buildm.uri.split('/').pop() + '.ttl');
-      // console.log('Writing file: ' + outputFile);
+    var basePath = '/tmp/duraark-data' + dataset.fileBaseUrl + '/';
 
-      try {
-        fs.writeFileSync(outputFile, ntripleString);
-      } catch (err) {
-        console.log('Error writing file %s: %s', outputFile, err);
-      }
+    converter.getDigitalObjectsUrls(basePath, '/tmp/duraark-data', 'http://duraark.tib.eu').then(function(urls) {
+      _.forEach(urls, function(url) {
+        console.log('processing URL: ' + url);
 
-      // var lines = ntripleString.split('.\n');
-      // _.forEach(lines, function(line) {
-      //   console.log('     ' + line);
-      // });
-    }).catch(function(err) {
-      console.log(err);
+        var doJsonLD = converter.createDigitalObjectAsJsonLD(url, paJsonLD.uri);
+
+        if (!paJsonLD.jsonld['http://data.duraark.eu/vocab/buildm/isRepresentedBy']) {
+          paJsonLD.jsonld['http://data.duraark.eu/vocab/buildm/isRepresentedBy'] = [];
+        }
+
+        paJsonLD.jsonld['http://data.duraark.eu/vocab/buildm/isRepresentedBy'].push({
+          '@value': doJsonLD.uri
+        });
+
+        dosJsonLD.push(doJsonLD.jsonld);
+      });
+
+      var tmp = [paJsonLD.jsonld];
+      _.forEach(dosJsonLD, function(item) {
+        tmp.push(item);
+      });
+
+      converter.writeRDFFile(tmp, paJsonLD.uri);
     });
   } else {
     console.log('Skipping bogus data from col: ' + col);
